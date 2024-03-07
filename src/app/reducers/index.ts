2994bddaa9c65ed37fe2c9a632f6f1e1dadd418f
Anderson Peñaloza <designeromega@gmail.com>
2024-03-07 09:16:26 +0100

    chore: :sparkles: add ngrx initial configuration
    Signed-off-by: Anderson Peñaloza <designeromega@gmail.com>

---
+++ src/app/reducers/index.ts
@@ -0,0 +1,17 @@
+import {
+  ActionReducer,
+  ActionReducerMap,
+  createFeatureSelector,
+  createSelector,
+  MetaReducer
+} from '@ngrx/store';
+import { environment } from '../../environments/environment';
+import { routerReducer } from '@ngrx/router-store';
+
+export interface AppState {}
+
+export const reducers: ActionReducerMap<AppState> = {};
+
+export const metaReducers: MetaReducer<AppState>[] = !environment.production
+  ? []
+  : [];
