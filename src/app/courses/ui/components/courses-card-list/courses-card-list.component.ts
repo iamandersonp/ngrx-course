1301b55768e8569a375243eb3ac9fe38bb74433c
Anderson Peñaloza <designeromega@gmail.com>
2024-03-11 15:06:55 +0100

    feat: :sparkles: standalone components implementation
    Signed-off-by: Anderson Peñaloza <designeromega@gmail.com>

--- src/app/courses/ui/components/courses-card-list/courses-card-list.component.ts
+++ src/app/courses/ui/components/courses-card-list/courses-card-list.component.ts
@@ -5,11 +5,18 @@ import { Course } from '../../../domain/model/course';
 import { EditCourseDialogComponent } from '../edit-course-dialog/edit-course-dialog.component';
 import { defaultDialogConfig } from '../../shared/default-dialog-config';
 import { CoursesEntityService } from '../../../infrastructure/courses-entity.service';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-courses-card-list',
   templateUrl: './courses-card-list.component.html',
-  styleUrls: ['./courses-card-list.component.css']
+  styleUrls: ['./courses-card-list.component.css'],
+  standalone: true,
+  imports: [NgFor, MatCardModule, MatButtonModule, RouterLink, MatIconModule]
 })
 export class CoursesCardListComponent {
   @Input() courses: Course[];
--- src/app/courses/ui/components/home/home.component.ts
+++ src/app/courses/ui/components/home/home.component.ts
@@ -8,11 +8,24 @@ import { defaultDialogConfig } from '../../shared/default-dialog-config';
 import { EditCourseDialogComponent } from '../edit-course-dialog/edit-course-dialog.component';
 
 import { CoursesEntityService } from '../../../infrastructure/courses-entity.service';
+import { AsyncPipe } from '@angular/common';
+import { CoursesCardListComponent } from '../courses-card-list/courses-card-list.component';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  standalone: true,
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatTabsModule,
+    CoursesCardListComponent,
+    AsyncPipe
+  ]
 })
 export class HomeComponent implements OnInit {
   private dialog: MatDialog = inject(MatDialog);
