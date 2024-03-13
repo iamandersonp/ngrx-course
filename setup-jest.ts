fb10cd3f20b0c739d333fb54ff74ea130f367838
Anderson Peñaloza <designeromega@gmail.com>
2024-03-13 09:02:23 +0100

    chore: :recycle: refactor eslint rules
    M	.eslintrc.json
    M	.git-hooks/pre-commit
    M	cypress/e2e/spec.cy.ts
    M	cypress/support/component-index.html
    M	cypress/support/component.ts
    M	package-lock.json
    M	package.json
    A	tsconfig.e2e.json
    M	tsconfig.json
    
    Signed-off-by: Anderson Peñaloza <designeromega@gmail.com>

--- setup-jest.ts
+++ setup-jest.ts
@@ -4,3 +4,17 @@ import 'jest-preset-angular/setup-jest';
 
 /* output shorter and more meaningful Zone error stack traces */
 // Error.stackTraceLimit = 2;
+
+Object.defineProperty(window, 'CSS', { value: null });
+Object.defineProperty(window, 'getComputedStyle', {
+  value: () => {
+    return {
+      display: 'none',
+      appearance: ['-webkit-appearance']
+    };
+  }
+});
+
+Object.defineProperty(document, 'doctype', {
+  value: '<!DOCTYPE html>'
+});
